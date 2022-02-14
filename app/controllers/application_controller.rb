febe8f4b820f06a33fd3d8b55fe0fd4dcd7a59f1
Oli Finn <oliver.james.finn@gmail.com>
2022-02-14 12:33:58 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  def ask
+  end
+  def answer
+    # @answer = answer
+    if params[:ask]
+        @question = params[:ask]
+      if params[:ask] == "I am going to work"
+        @answer = "Great!"
+      elsif params[:ask].end_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
 end
